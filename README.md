d0a897cadb281d2ee1fa7b24462a3441ccb435d6
Nelson Nobre <technobre@gmail.com>
2022-06-18 15:57:24 +0100

    Kickoff

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# example-generate-random-contacts
+# Example generate random contacts
 Example how you can generate a list of random contacts
+
+
+
+## Dependencies
+
+- [Bogus](https://www.nuget.org/packages/Bogus) to generate fake data
+- [CsvHelper](https://www.nuget.org/packages/CsvHelper) to export contacts to CSV
+
+
+
+## Run project
+
+`dotnet run <number of contacts>`
+
+```csharp
+cd src
+dotnet run 10000
+```
---
+++ src/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+using Bogus;
+using Bogus.DataSets;
+using CsvHelper;
+using static System.Console;
+
+
+var totalContacts = args.Length > 0 ? Convert.ToInt32(args[0]) : 10;
+
+WriteLine($"Generate {totalContacts} contacts");
+WriteLine();
+
+
+var contacts = new Dictionary<string, Contact>();
+
+
+while(contacts.Count < totalContacts)
+{
+    var gender = new Faker().PickRandom<Name.Gender>();
+
+    var dataFake = new Faker<Contact>()
+        .CustomInstantiator(f => new()
+        {
+            Name = f.Name.FullName(gender),
+            Gender = gender.ToString().ToUpper(),
+            DateOfBirth = f.Date.Past(100, DateTime.UtcNow.AddYears(-18)).ToString("yyyy-MM-dd"),
+            Country = f.Address.CountryCode(),
+            Address = f.Address.FullAddress()
+        })
+        .RuleFor(r => r.Email, (f, c) => f.Internet.Email(c.Name)?.ToLower());
+
+    var contact = dataFake.Generate();
+
+
+    if(contacts.ContainsKey(contact.Email))
+    { // Prevent duplicated emails
+        WriteLine($"DUPLICATED: {contact.Email}");
+        continue;
+    }
+
+
+    contacts.Add(contact.Email, contact);
+    WriteLine($"ADDED: {contact} - TOTAL: {contacts.Count}");
+}
+
+
+using(var writer = new StreamWriter("contacts.csv"))
+using(var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(contacts.Values);
+}
+
+WriteLine();
+WriteLine("***** END *****");
+
+
+
+public record Contact
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Gender { get; set; }
+    public string? DateOfBirth { get; set; }
+
+    public string? Country { get; set; }
+    public string? Address { get; set; }
+}
